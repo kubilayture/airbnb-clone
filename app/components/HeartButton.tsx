a5a6865bfb66777d337d61b80833f93150c5abd7
Kubilay Türe <kubilayture@gmail.com>
2023-10-05 21:59:44 +0300

    fixed style

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -25,7 +25,7 @@ const HeartButton: React.FC<HeartButtonProps> = ({
         className='fill-white absolute -top-[2px] -right-[2px]'
       />
       <AiFillHeart
-        size={28}
+        size={24}
         className={hasFavorited ? 'fill-rose-500' : 'fill-neutral-500/70'}
       />
     </div>
