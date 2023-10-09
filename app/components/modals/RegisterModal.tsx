5b2d16d08b216b68c785691e5f8211a6596bef55
Kubilay Türe <kubilayture@gmail.com>
2023-10-09 21:40:11 +0300

    success message on register

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -38,6 +38,7 @@ const RegisterModal = () => {
 
     axios.post('/api/register', data)
       .then(() => {
+        toast.success('Register is Success!')
         registerModal.onClose()
         loginModal.onOpen()
       })
